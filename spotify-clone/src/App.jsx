d5abb6388c7e10fd9030648648b8a3bb538e018f
sdpee <steven.powell@gmx.com>
2024-09-17 17:20:30 +0100

    Sidebar stage 1

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Sidebar } from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div>
-
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
